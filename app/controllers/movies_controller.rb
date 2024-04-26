3da5665f1505effe565b3f43942624b245f13638
luricl <luri.cl95@gmail.com>
2024-04-26 19:12:10 -0300

    create functionalities for creating movies and update controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,45 @@
 class MoviesController < ApplicationController
-    
+    # 'index' and 'show' methods from Section 4.4 omitted for clarity
     def index
-      @movies = Movie.all
+        @movies = Movie.all
     end
-
     def show
-      id = params[:id]     
-      @movie = Movie.find(id)
+        id = params[:id]     
+        @movie = Movie.find(id)
+    end  
+    def new
+        @movie = Movie.new
+    end 
+    def create
+        if (@movie = Movie.create(movie_params))
+        redirect_to movies_path, :notice => "#{@movie.title} created."
+        else
+        flash[:alert] = "Movie #{@movie.title} could not be created: " +
+            @movie.errors.full_messages.join(",")
+        render 'new'
+        end
     end
-  end
+    def edit
+        @movie = Movie.find params[:id]
+    end
+    def update
+        @movie = Movie.find params[:id]
+        if (@movie.update_attributes(movie_params))
+        redirect_to movie_path(@movie), :notice => "#{@movie.title} updated."
+        else
+        flash[:alert] = "#{@movie.title} could not be updated: " +
+            @movie.errors.full_messages.join(",")
+        render 'edit'
+        end
+    end
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_path, :notice => "#{@movie.title} deleted."
+    end
+    private
+    def movie_params
+        params.require(:movie)
+        params[:movie].permit(:title,:rating,:release_date)
+    end
+end
